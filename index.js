c5659a60f1964f24c84447eb1ac3f5bea728a470
MarChurra <126945275+MarChurra@users.noreply.github.com>
2024-05-16 09:44:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+const colorPicker = document.getElementById('colorPicker')
+const selectElement = document.getElementById('select')
+
+let chosenColor = ''
+let selectedScheme = ''
+
+//Trigger Default Choices
+document.addEventListener('DOMContentLoaded', function () {
+    handleSelectedScheme()
+    chosenColor = colorPicker.value.replace('#', '');
+})
+
+//Get Chosen Color
+colorPicker.addEventListener('change', watchColorPicker)
+
+function watchColorPicker(e) {
+    chosenColor = (e.target.value.replace('#', ''))
+}
+
+//handle the select Element Values
+selectElement.addEventListener('change', handleSelectedScheme)
+
+function handleSelectedScheme() {
+    selectedScheme = selectElement.value
+}
+
+//Generate the Colors Cols
+document.getElementById('btn').addEventListener('click', function () {
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${selectedScheme}`)
+
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok')
+            }
+
+            return res.json();
+        })
+
+        .then(data => {
+            let columnHTML = ''
+            data.colors.forEach(function (color, index) {
+                columnHTML +=
+                    `
+                <div class="col-wrapper" style="animation-delay: ${index * 0.1}s">
+                <img class="color-cols fade-in" src="${color.image.named}">
+                <h2>${color.hex.value}</h2>
+                </div>
+                `
+            })
+
+            document.getElementById('colorCols').innerHTML = columnHTML
+            copyToClipboard()
+        })
+        .catch(error => console.error('There was a problem with the fetch operation:', error))
+})
+
+
+
+
+//Copy to Clipboard
+function copyToClipboard() {
+    const columns = document.querySelectorAll('.color-cols')
+    columns.forEach(column => {
+        console.log(column)
+        column.addEventListener('click', function () {
+            const hexValue = column.nextElementSibling.textContent
+            console.log(hexValue)
+            navigator.clipboard.writeText(hexValue)
+                .then(() => {
+                    window.alert('Hex value copied to clipboard')
+                })
+                .catch(err => {
+                    console.error('Failed to copy text: ', err)
+                })
+        })
+    })
+}
